e19f40d1fc4d8d99345a00a39d096bab6804efaa
Anandhu_P_A <anandhupa131@gmail.com>
2023-03-24 22:42:16 +0530

    done 22

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-console.log("index.js running successfully");
+//console.log("index.js running successfully")
 
-const shortid = require("shortid");
-console.log(shortid.generate());
+// const shortid = require("shortid")
+// console.log(shortid.generate())
+
+console.log('check with single quotes')
